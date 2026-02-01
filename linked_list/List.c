8a016cd48d0cc84c8cbbced3d74eda7e09e0049a
Choria <2963482580qq.com>
2026-02-01 21:36:20 +0800

    2026年2月1日21:36:09 study list

--- linked_list/List.c
+++ linked_list/List.c
@@ -11,16 +11,97 @@ typedef struct {
     int length;
 } SeqList;
 
-void initList(SeqList *list){
+
+
+//序列为空初始化列表
+void initList(SeqList *list){ 
    list->length = 0;
 }
 
+
+//在尾部添加元素
+int appendElem(SeqList *list, ElemType e){
+   if(list->length >= MAXSIZE){
+     printf("顺序表已满\n");
+ return 0;
+   }
+
+   list->data[list->length] = e;
+   list->length++;
+
+   return 0;
+}
+
+//中间插入元素
+
+int insertElem(SeqList * list,int l,ElemType e){
+
+
+
+
+   if(l<=list->length){
+     
+       for(int i = list->length - 1;i>= l-1;i--){
+     list->data[i+1] = list->data[i];
+   }
+
+         list->data[l] = e;
+     list->length++;
+
+   
+     return 1;
+   }
+
+}
+
+
+//遍历输出元素
+void printList(SeqList *list){
+   
+   for(int i =0;i<list->length ; i++ ){
+     printf("%d\n",list->data[i]);
+   }
+
+    printf("\n");
+}
+
+
+//删除元素;
+
+void deleteElem(SeqList *list, int l,ElemType *e){
+   
+
+   *e = list->data[l-1]; //1.保存要删除的元素;
+
+    if(l<=list->length){ 
+    
+      for(int i = l;i<list->length;i++){
+         list->data[i-1] = list->data[i]; //向前赋值
+      }
+      
+      list->length--;
+    }
+
+    return;
+}
+
+
 int main(){
   SeqList list;
-  initList(&list);
-
+  ElemType delnum;
+  initList(&list); 
   printf("list's length-> %d\n", list.length);
-  printf("list's size %zu",sizeof(list.data));
+  printf("list's size %zu\n",sizeof(list.data));
 
+   appendElem(&list,23);
+   appendElem(&list,10);
+
+
+  insertElem(&list,2,88); 
+  insertElem(&list,2,66); 
+
+
+   deleteElem(&list,2,&delnum);
+  printList(&list);
   return 0;
 }
