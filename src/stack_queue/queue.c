7fcda2dae2edf0638ed1b6342ed52f0f96f844bd
Choria <2963482580qq.com>
2026-02-08 17:54:39 +0800

    start queue

---
+++ src/stack_queue/queue.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXSIZE 100
+
+typedef int ElemType;
+//新建队列结构体
+typedef struct 
+{
+   ElemType data[MAXSIZE];
+   
+   int front;
+   int rear;
+   int size;
+  /* data */
+} Queue;
+
+
+Queue*  initqueue(){ 
+ 
+
+  Queue *q = (Queue* )malloc(sizeof(Queue));
+   
+  ElemType* e = (ElemType* )malloc(sizeof(ElemType) * MAXSIZE);
+
+q->front = 0;
+q->rear = 0;
+q->size = 0;
+ 
+   return q; 
+}
+
+
+int main(){
+   
+
+  return 0;
+}
