e529b61137ce3bc7fd687228f0c479541831c6fa
Choria <2963482580qq.com>
2026-02-08 14:27:41 +0800

    changed file structrue and add stack_queue file

---
+++ src/stack_queue/stack.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//简单实现二叉树
+
+
+
+
+typedef int Elemtype;
+
+typedef struct  {
+    struct *treeNode left;
+    struct *treeNode rigth;
+    Elemtype data;
+} treeNode
+
+
+int craeteTree(treeNode *node,Elemtype e){
+      
+} 
+
+
+int main(){ 
+
+
+  return 0;
+
+}
